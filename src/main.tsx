5238025dd3881249eb3e6064eda6f6fa627d53d9
konor <emilioxkonor@gmail.com>
2025-04-20 16:57:36 -0600

    Update react-scan to v0.1.4 and use module import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+import { scan } from "react-scan";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+scan({ enabled: true });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
