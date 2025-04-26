1c723afe370bf56c7956e9728765cddbd7ac8cec
konor <emilioxkonor@gmail.com>
2025-04-25 21:16:46 -0600

    Remove react-scan and downgrade Vite packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
-import { scan } from "react-scan";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-scan({ enabled: true });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
