392a69815a712468e322bf57b27d81981671995c
konor <emilioxkonor@gmail.com>
2025-04-25 21:15:19 -0600

    Refactor header with modular Modbus player component

---
+++ src/components/initial-values-form.tsx
@@ -0,0 +1,125 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useData } from "@/hooks/useData";
+import { useVariables } from "@/hooks/useVariables";
+import { cn } from "@/lib/utils";
+import { Info } from "lucide-react";
+import { useEffect } from "react";
+
+const formSchema = z.object({
+  initialMass: z.coerce
+    .number({ invalid_type_error: "Must be a number" })
+    .positive("The value must be positive"),
+  initialComposition: z.coerce
+    .number({ invalid_type_error: "Must be a number" })
+    .min(0, "The minimum value is 0")
+    .max(100, "The maximum value is 1"),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+interface InitialValuesFormProps {
+  className?: string;
+  defaultValues?: Partial<FormValues>;
+}
+
+export function InitialValuesForm({
+  className,
+  defaultValues = { initialMass: undefined, initialComposition: undefined },
+}: InitialValuesFormProps) {
+  const connected = useData((state) => state.connected);
+  const initialMass = useVariables((state) => state.initialMass);
+  const initialComposition = useVariables((state) => state.initialComposition);
+
+  useEffect(() => {
+    if (connected !== "file") {
+      form.reset();
+      return;
+    }
+    form.setValue("initialMass", initialMass || 0);
+    form.setValue("initialComposition", initialComposition || 0);
+  }, [initialMass, initialComposition, connected]);
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  });
+
+  const handleSubmit = async (values: FormValues) => {};
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className={cn("space-y-2", className)}
+      >
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <FormField
+            control={form.control}
+            name="initialMass"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="flex items-center gap-2">
+                  Masa inicial (g)
+                  <Info className="size-3 self-start" />
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Ej: 100"
+                    {...field}
+                    value={field.value || ""}
+                    type="number"
+                    step="0.01"
+                    disabled={connected === "file"}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="initialComposition"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="flex items-center gap-2">
+                  Composición (%m/m)
+                  <Info className="size-3 self-start" />
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Ej: 0.89"
+                    {...field}
+                    value={field.value || ""}
+                    type="number"
+                    step="0.01"
+                    min="0"
+                    max="100"
+                    disabled={connected === "file"}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button
+          type="submit"
+          className="w-full"
+          disabled={connected === "file"}
+        >
+          Set Variables
+        </Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/components/modbus-player.tsx
@@ -0,0 +1,81 @@
+import { logger } from "@/adapters/tauri";
+import { commands } from "@/bindings";
+import { Button } from "@/components/ui/button";
+import { useData } from "@/hooks/useData";
+import { useVariables } from "@/hooks/useVariables";
+import { cn } from "@/lib/utils";
+import { Power, Save, Settings } from "lucide-react";
+import { toast } from "sonner";
+import { ExportDialog } from "./export-dialog";
+import { SettingsDialog } from "./settings-dialog";
+import { StatusLed } from "./ui/status-led";
+
+export function ModbusPlayer() {
+  const connected = useData((state) => state.connected);
+  const setConnected = useData((state) => state.setConnected);
+  const clearData = useData((state) => state.clearData);
+  const numberPlates = useVariables((state) => state.numberPlates);
+
+  const handleConnection = async () => {
+    if (connected === "modbus") {
+      const response = await commands.disconnectModbus();
+      if (response.status !== "ok") {
+        logger.error(`Error disconnecting from MODBUS: ${response.error}`);
+        setConnected("modbus");
+        return;
+      }
+      setConnected("none");
+      clearData();
+    } else {
+      const connectModbus = async () => {
+        const response = await commands.connectModbus();
+        if (response.status !== "ok") {
+          throw new Error(response.error.type);
+        }
+      };
+
+      toast.promise(
+        connectModbus().then(() => {
+          setConnected("modbus");
+          commands.sendColumnData(
+            numberPlates,
+            0, // TODO
+            0,
+          );
+        }),
+        {
+          loading: "Connecting to MODBUS...",
+          success: "Connected to MODBUS",
+          error: "Error connecting to MODBUS",
+        },
+      );
+    }
+  };
+
+  return (
+    <div className="flex items-center">
+      <SettingsDialog>
+        <Button size={"icon"} className="rounded-r-none">
+          <Settings className="h-4 w-4" />
+        </Button>
+      </SettingsDialog>
+      <Button
+        className={cn(
+          "rounded-none border-x-[0.1px] border-x-gray-700",
+          connected === "modbus" && "bg-red-500",
+        )}
+        onClick={handleConnection}
+      >
+        <Power className="h-4 w-4" />
+        Connect MODBUS
+      </Button>
+      <ExportDialog>
+        <Button className="rounded-l-none">
+          <Save className="h-4 w-4" />
+          Save
+        </Button>
+      </ExportDialog>
+      <StatusLed connected={connected === "modbus"} className="ml-4" />
+    </div>
+  );
+}
